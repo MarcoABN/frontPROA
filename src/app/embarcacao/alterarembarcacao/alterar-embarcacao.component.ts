7f3f0c987604c6fa5af34231dda1a624e6c02bf6
MarcoABN <marcoanunes23@gmail.com>
2024-06-04 13:52:11 -0300

    Commit antes de iniciar tratativa de PDF

--- src/app/embarcacao/alterarembarcacao/alterar-embarcacao.component.ts
+++ src/app/embarcacao/alterarembarcacao/alterar-embarcacao.component.ts
@@ -1,53 +1,60 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FrontEmbarcacaoService } from 'src/app/services/front-embarcacao.service';
+import { CepService } from 'src/app/services/cep.service'; // Importando o serviço de CEP
 import { Embarcacao } from 'src/app/model/embarcacao';
-
 
 @Component({
   selector: 'app-alterar-embarcacao',
   templateUrl: './alterar-embarcacao.component.html',
   styleUrls: ['./alterar-embarcacao.component.css']
 })
+export class AlterarEmbarcacaoComponent implements OnInit {
 
+  idEmbarcacao!: number;
+  embarcacao!: Embarcacao;
 
+  constructor(
+    private embarcacaoService: FrontEmbarcacaoService, 
+    private cepService: CepService, // Injetando o serviço de CEP
+    private router: Router, 
+    private route: ActivatedRoute
+  ) {}
 
-export class AlterarEmbarcacaoComponent {
+  ngOnInit(): void {
+    this.idEmbarcacao = this.route.snapshot.params['id'];
+    this.embarcacao = new Embarcacao();
+    this.embarcacaoService.consultarEmbarcacao(this.idEmbarcacao).subscribe(data => {
+      this.embarcacao = data;
+    });
+  }
 
+  retornar() {
+    this.router.navigate(['/embarcacao/listarembarcacao']);
+  }
 
-  idEmbarcacao! :number;
-  embarcacao! :Embarcacao;
+  onSubmit() {
+    this.embarcacaoService.alterarEmbarcacao(this.idEmbarcacao, this.embarcacao).subscribe(data => {
+      console.log(data);
+      this.retornar();
+    });
+  }
 
-  constructor(private embarcacaoService :FrontEmbarcacaoService, private router: Router, private route: ActivatedRoute){}
-
-    ngOnInit() :void{
-      this.idEmbarcacao = this.route.snapshot.params['id'];
-      this.embarcacao = new Embarcacao();
-      this.embarcacaoService.consultarEmbarcacao(this.idEmbarcacao).subscribe (data => {
-        this.embarcacao = data;
-      })
+  buscarEndereco() {
+    if (this.embarcacao.CEP) {
+      this.cepService.buscarCep(this.embarcacao.CEP).subscribe(
+        dados => {
+          if (dados) {
+            this.embarcacao.logradouro = dados.logradouro;
+            this.embarcacao.bairro = dados.bairro;
+            this.embarcacao.UF = dados.uf;
+            this.embarcacao.cidade = dados.localidade;
+          }
+        },
+        error => {
+          console.error('Erro ao buscar o endereço:', error);
+        }
+      );
     }
-
-    retornar(){
-      this.router.navigate(['/embarcacao/listarembarcacao']);
-    }
-
-    onSubmit(){
-
-      //if (!this.embarcacao.nome || !this.embarcacao.dtFab || !this.embarcacao.potencia) {
-      //  alert("Por favor, preencha todos os campos.");
-      //  return;
-      //}
-      
-
-
-      this.embarcacaoService.alterarEmbarcacao(this.idEmbarcacao, this.embarcacao).subscribe (data => {
-        console.log(data);
-        this.retornar();
-      })
-    }
-    
-
+  }
 }
-
--- src/app/embarcacao/cadastrarembarcacao/cadastrar-embarcacao.component.ts
+++ src/app/embarcacao/cadastrarembarcacao/cadastrar-embarcacao.component.ts
@@ -1,39 +1,60 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Embarcacao } from 'src/app/model/embarcacao';
 import { FrontEmbarcacaoService } from 'src/app/services/front-embarcacao.service';
+import { CepService } from 'src/app/services/cep.service';
 
 @Component({
   selector: 'app-cadastrar-embarcacao',
   templateUrl: './cadastrar-embarcacao.component.html',
   styleUrls: ['./cadastrar-embarcacao.component.css']
 })
-export class CadastrarEmbarcacaoComponent {
+export class CadastrarEmbarcacaoComponent implements OnInit {
 
-    idEmbarcacao!: number;
-    embarcacao: Embarcacao = new Embarcacao();
+  idEmbarcacao!: number;
+  embarcacao: Embarcacao = new Embarcacao();
 
-    constructor(private embarcacaoService: FrontEmbarcacaoService, private router: Router){}
+  constructor(
+    private embarcacaoService: FrontEmbarcacaoService,
+    private router: Router,
+    private cepService: CepService
+  ){}
 
-      ngOnInit(): void{}
+  ngOnInit(): void {}
 
-      retornar(){
-        this.router.navigate(['inicio']);
-      }
+  retornar(){
+    this.router.navigate(['inicio']);
+  }
 
-      onSubmit(){
+  onSubmit(){
+    if (!this.embarcacao.nomeEmbarcacao || !this.embarcacao.dtConstrucao) {
+      alert("Por favor, preencha todos os campos.");
+      return;
+    }
 
-        if (!this.embarcacao.nomeEmbarcacao || !this.embarcacao.dtConstrucao) {
-          alert("Por favor, preencha todos os campos.");
-          return;
+    this.embarcacao.id = 0;
+
+    this.embarcacaoService.cadastrarEmbarcacao(this.embarcacao).subscribe(data => {
+      console.log(data);
+      this.retornar();
+    });
+  }
+
+  buscarEndereco() {
+    if (this.embarcacao.CEP) {
+      this.cepService.buscarCep(this.embarcacao.CEP).subscribe(
+        dados => {
+          if (dados) {
+            this.embarcacao.logradouro = dados.logradouro;
+            this.embarcacao.bairro = dados.bairro;
+            this.embarcacao.UF = dados.uf;
+            this.embarcacao.cidade = dados.localidade;
+          }
+        },
+        error => {
+          console.error('Erro ao buscar o endereço:', error);
         }
-
-        this.embarcacao.id = 0;
-
-        this.embarcacaoService.cadastrarEmbarcacao(this.embarcacao).subscribe (data => {
-          console.log(data);
-          this.retornar();
-        });
-      }
-
+      );
+    }
+  }
 }
